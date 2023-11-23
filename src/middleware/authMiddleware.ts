8d2b7727d7fdf79b5e3d120aec6140566fc10296
Ahsan Shahid <ahsanshahid2882@gmail.com>
2023-11-24 01:23:05 +0500

    improve code

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -12,7 +12,7 @@ export const authenticateToken = async (
     return res.status(401).json({ message: "Unauthorized: Token missing" });
   }
 
-  jwt.verify(token, process.env.SECRET_JWT_TOKEN, async (err, _) => {
+  jwt.verify(token, process.env.SECRET_JWT_TOKEN, async (err) => {
     if (err) {
       return res.status(403).json({
         message: "Unauthorized: Invalid token",
